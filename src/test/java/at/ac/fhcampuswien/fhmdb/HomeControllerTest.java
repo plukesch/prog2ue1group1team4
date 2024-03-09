d7dbb092a723101d592214b960640f06218afcd4
plukesch <patrick.lukesch@chello.at>
2024-03-09 16:54:38 +0100

    first looks with testcases

---
+++ src/test/java/at/ac/fhcampuswien/fhmdb/HomeControllerTest.java
@@ -0,0 +1,12 @@
+package at.ac.fhcampuswien.fhmdb;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class HomeControllerTest {
+
+    @Test
+    void testApplyFilters() {
+
+    }
+}
