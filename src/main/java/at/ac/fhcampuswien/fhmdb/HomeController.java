4471ecfbec1d642d3e68a1ad361b1444273fe3a0
Huawei <alfonso.de.vera@stud.fh-campuswien.ac.at>
2024-03-09 14:55:48 +0100

    Alphabetized Genres

--- src/main/java/at/ac/fhcampuswien/fhmdb/HomeController.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/HomeController.java
@@ -47,7 +47,7 @@ public class HomeController implements Initializable {
         // TODO add genre filter items with genreComboBox.getItems().addAll(...)
         genreComboBox.setPromptText("Filter by Genre");
 
-        genreComboBox.getItems().addAll("ACTION", "COMEDY", "DRAMA", "CRIME", "SCIENCE-FICTION", "SPORT", "THRILLER", "MYSTERY", "ROMANCE", "MUSICAL", "WESTERN", "ADVENTURE");
+        genreComboBox.getItems().addAll("ACTION", "ADVENTURE", "COMEDY", "CRIME", "DRAMA", "MUSICAL", "MYSTERY", "ROMANCE", "SCIENCE-FICTION", "SPORT", "THRILLER", "WESTERN");
 
         // TODO add event handlers to buttons and call the regarding methods
         // either set event handlers in the fxml file (onAction) or add them here
