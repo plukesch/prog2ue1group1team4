c2dce3488459795a12153b2348442bbd2bb7919b
Huawei <alfonso.de.vera@stud.fh-campuswien.ac.at>
2024-03-09 17:18:15 +0100

    Added testGetGenres

--- src/test/java/at/ac/fhcampuswien/fhmdb/models/MovieTest.java
+++ src/test/java/at/ac/fhcampuswien/fhmdb/models/MovieTest.java
@@ -2,6 +2,7 @@ package at.ac.fhcampuswien.fhmdb.models;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -24,4 +25,22 @@ class MovieTest {
         //when & then
         assertEquals("Based on the true story of Jordan Belfort, from his rise to a wealthy stock-broker living the high life to his fall involving crime, corruption and the federal government.", movie.getDescription());
     }
+
+    @Test
+    public void testGetGenres() {
+        //given
+        List<Genre> expectedGenres = new ArrayList<>();
+        expectedGenres.add(new Genre("Science Fiction"));
+        expectedGenres.add(new Genre("Adventure"));
+
+        Movie movie = new Movie("Dune", "It is the first of a two-part adaptation of the 1965 novel of the same name by Frank Herbert. Set in the distant future, the film follows Paul Atreides as his family, the noble House Atreides, is thrust into a war for the deadly and inhospitable desert planet Arrakis.", expectedGenres);
+
+        //when & then
+        List<Genre> actualGenres = movie.getGenres();
+        assertEquals(expectedGenres.size(), actualGenres.size());
+        for (int i = 0; i < expectedGenres.size(); i++) {
+            assertEquals(expectedGenres.get(i).getName(), actualGenres.get(i).getName());
+        }
+    }
+
 }
