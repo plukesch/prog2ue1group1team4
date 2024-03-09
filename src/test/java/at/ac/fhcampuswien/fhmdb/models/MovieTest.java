3a426074361223aab80a77ff6f271fb5c03590ef
Huawei <alfonso.de.vera@stud.fh-campuswien.ac.at>
2024-03-09 17:20:50 +0100

    Fixed Typos

--- src/test/java/at/ac/fhcampuswien/fhmdb/models/MovieTest.java
+++ src/test/java/at/ac/fhcampuswien/fhmdb/models/MovieTest.java
@@ -11,31 +11,31 @@ class MovieTest {
 
     @Test
     public void testGetTitle() {
-        //given
+        //Given
         Movie movie = new Movie("The Wolf of Wall Street", "Based on the true story of Jordan Belfort, from his rise to a wealthy stock-broker living the high life to his fall involving crime, corruption and the federal government.", List.of(new Genre("Crime"), new Genre("Comedy")));
 
-        //when & then
+        //When & Then
         assertEquals("The Wolf of Wall Street", movie.getTitle());
     }
     @Test
     public void testGetDescription() {
-        //given
+        //Given
         Movie movie = new Movie("The Wolf of Wall Street", "Based on the true story of Jordan Belfort, from his rise to a wealthy stock-broker living the high life to his fall involving crime, corruption and the federal government.", List.of(new Genre("Crime"), new Genre("Comedy")));
 
-        //when & then
+        //When & Then
         assertEquals("Based on the true story of Jordan Belfort, from his rise to a wealthy stock-broker living the high life to his fall involving crime, corruption and the federal government.", movie.getDescription());
     }
 
     @Test
     public void testGetGenres() {
-        //given
+        //Given
         List<Genre> expectedGenres = new ArrayList<>();
         expectedGenres.add(new Genre("Science Fiction"));
         expectedGenres.add(new Genre("Adventure"));
 
         Movie movie = new Movie("Dune", "It is the first of a two-part adaptation of the 1965 novel of the same name by Frank Herbert. Set in the distant future, the film follows Paul Atreides as his family, the noble House Atreides, is thrust into a war for the deadly and inhospitable desert planet Arrakis.", expectedGenres);
 
-        //when & then
+        //When & Then
         List<Genre> actualGenres = movie.getGenres();
         assertEquals(expectedGenres.size(), actualGenres.size());
         for (int i = 0; i < expectedGenres.size(); i++) {
